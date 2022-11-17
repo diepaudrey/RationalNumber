2d46ff6c6d3b9ee1804f8b3a134d18701e89f111
Audrey portable <diep.audrey@gmail.com>
2022-11-17 09:36:17 +0100

    gitignore added

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # RationalNumber
 Project created by COUARD Lou and DIEP Audrey
+
+---
+
+* you have to be in the folder /RationalNumber
+How to run : 
+
+mkdir build 
+cd build 
+cmake .. 
+./bin/myExamples
--- myExamples/src/rationalTest.cpp
+++ myExamples/src/rationalTest.cpp
@@ -1,8 +1,24 @@
-//haaaaa
-#include "rationalTest.hpp" 
+
+#include "rational.hpp" 
 #include <iostream>
 
 int main(){
-    std::cout << "hello" << std::endl;
+    
+    // //initialize tests
+    // Rational<int> fraction(2,5);
+    // std::cout << "numerateur : " << fraction.m_numerator << std::endl;
+    // std::cout << "deno : " << fraction.m_denominator << std::endl;
+
+    // //copy constructor
+    // Rational<int> fraction2(fraction);
+    // std::cout << "numerateur2 : " << fraction2.m_numerator << std::endl;
+    // std::cout << "deno2 : " << fraction2.m_denominator << std::endl;
+
+    // Rational<int> fraction3;
+    // std::cout << "numerateur3 : " << fraction3.m_numerator << std::endl;
+    // std::cout << "deno3 : " << fraction3.m_denominator << std::endl;
+
+    std::cout << "ça marche ??" << std::endl;
+
     return 0;
 }
